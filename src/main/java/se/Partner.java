9ad7d7185a9ad0b02aa19e003b80c455424d24da
timojkankaanpaa <tka@puv.fi>
2017-03-17 10:06:27 +0200

    first commit

---
+++ src/main/java/se/Partner.java
@@ -0,0 +1,41 @@
+package se;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the partner database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Partner.findAll", query="SELECT p FROM Partner p")
+public class Partner implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	public Partner() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
