812994407647c168d40e22f12d3463c720559cd0
timojkankaanpaa <tka@puv.fi>
2017-03-17 10:15:14 +0200

    Added SQL for table

--- src/main/java/se/Partner.java
+++ src/main/java/se/Partner.java
@@ -3,7 +3,11 @@ package se;
 import java.io.Serializable;
 import javax.persistence.*;
 
-
+//to enable you to create a compatible datbase table:
+//CREATE TABLE IF NOT EXISTS `partner` (`id` int(11) NOT NULL,`name` varchar(10) DEFAULT NULL) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+//ALTER TABLE `partner` ADD PRIMARY KEY (`id`);
+//ALTER TABLE `partner` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+//INSERT INTO `partner` (`id`, `name`) VALUES (1, 'VAMK Oy'), (2, 'Timo');
 /**
  * The persistent class for the partner database table.
  * 
