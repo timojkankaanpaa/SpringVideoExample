5bd59fec9ba7e432a12482fb101cebe9daf89390
timojkankaanpaa <tka@puv.fi>
2017-11-10 15:59:04 +0200

    Added swagger support and implemented Municipality functionality

--- src/main/java/se/Application.java
+++ src/main/java/se/Application.java
@@ -2,7 +2,9 @@ package se;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import (SwaggerConfig.class) //for Swagger documentation - Swagger home http://localhost:8080/swagger-ui.html
 @SpringBootApplication
 public class Application {
 
--- src/main/java/se/Partner.java
+++ src/main/java/se/Partner.java
@@ -3,11 +3,7 @@ package se;
 import java.io.Serializable;
 import javax.persistence.*;
 
-//to enable you to create a compatible datbase table:
-//CREATE TABLE IF NOT EXISTS `partner` (`id` int(11) NOT NULL,`name` varchar(10) DEFAULT NULL) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-//ALTER TABLE `partner` ADD PRIMARY KEY (`id`);
-//ALTER TABLE `partner` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
-//INSERT INTO `partner` (`id`, `name`) VALUES (1, 'VAMK Oy'), (2, 'Timo');
+
 /**
  * The persistent class for the partner database table.
  * 
@@ -18,7 +14,8 @@ public class Partner implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@SequenceGenerator(name="PARTNER_ID_GENERATOR" )
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="PARTNER_ID_GENERATOR")
 	private int id;
 
 	private String name;
