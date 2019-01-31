cbb43317a7c560b8e391da46cb42abe508e0f254
Timo Kankaanpää <tka@puv.fi>
2019-01-31 21:07:19 +0200

    Cleaned JDK from pom.xml, removed /target

--- partner.sql
+++ partner.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u2
+-- version 4.2.12deb2+deb8u3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 20, 2017 at 09:10 AM
--- Server version: 5.5.55-0+deb8u1
--- PHP Version: 5.6.30-0+deb8u1
+-- Generation Time: Jan 31, 2019 at 09:05 PM
+-- Server version: 5.5.62-0+deb8u1
+-- PHP Version: 5.6.39-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `tka_2017`
+-- Database: `tka_video`
 --
 
 -- --------------------------------------------------------
