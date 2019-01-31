71852c7ae3867af3b7eaa95d8aaa3a05daad3bd6
Timo Kankaanpää <tka@puv.fi>
2019-01-31 21:23:09 +0200

    Working example fix username/pw

--- src/main/java/se/MunicipalityRepository.java
+++
@@ -1,7 +0,0 @@
-package se;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface MunicipalityRepository extends CrudRepository<Municipality, Integer> {
-
-}
--- src/main/java/se/PartnerController.java
+++ src/main/java/se/PartnerController.java
@@ -1,8 +1,12 @@
 package se;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +22,25 @@ public class PartnerController {
     public Iterable<Partner> partners(@RequestParam(value="name", defaultValue="World") String name) {
         return repository.findAll();
     }    
+
+    @RequestMapping("/partner/{id}")
+    public Partner get(@PathVariable("id") int id) {
+        return repository.findOne(id);
+    }    
+
+    @RequestMapping(value = "/partner" , method = RequestMethod.PUT)
+    public @ResponseBody Partner update(@RequestBody Partner item) {
+    	return repository.save(item);
+    } 
+    
+    @RequestMapping(value = "/partner" , method = RequestMethod.DELETE)
+    public void delete(@RequestBody Partner item) {
+    	repository.delete(item);
+        //do business logic
+    }     
+
+    @RequestMapping(value = "/partner" , method = RequestMethod.POST)
+    public @ResponseBody Partner create(@RequestBody Partner item) {
+    	return repository.save(item);
+    }  
 }
