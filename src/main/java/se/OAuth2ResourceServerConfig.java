a0898ee3678de5ddde153da48bde75a67e17cc7c
Timo Kankaanpää <tka@puv.fi>
2019-02-19 17:49:21 +0200

    Simple web client index.html

---
+++ src/main/java/se/OAuth2ResourceServerConfig.java
@@ -0,0 +1,28 @@
+@Configuration
+@EnableResourceServer
+public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(ResourceServerSecurityConfigurer config) {
+        config.tokenServices(tokenServices());
+    }
+ 
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+ 
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
+    }
+ 
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
+    }
+}
